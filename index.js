d491e8aa8bbd9a038ced1e516afee9ae51e2722c
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2026-04-12 10:30:57 +0200

    chore: build function

--- index.js
+++ index.js
@@ -6,19 +6,8 @@ async function docker_buildx() {
     try {
         checkPlatform();
         cloneMyself();
-        const imageName = getInput('imageName', { required: true });
         await executeShellScript('install_buildx');
-        const imageTag = getInput('tag');
-        const dockerFile = getInput('dockerFile');
-        const publish = getInput('publish').toLowerCase() === 'true';
-        const load = getInput('load').toLowerCase() === 'true';
-        const platform = getInput('platform');
-        const buildArg = getInput('buildArg');
-        const target = getInput('target');
-        const label = getInput('label');
-        const context = getInput('context');
-        const buildFunction = publish ? buildAndPublish : buildOnly;
-        await buildFunction(platform, imageName, imageTag, dockerFile, buildArg, label, load, context, target);
+        await build();
         cleanMyself();
     } catch (error) {
         setFailed(error.message);
@@ -37,28 +26,44 @@ async function executeShellScript(scriptName, envVars = {}) {
     child_process.execSync(command, { stdio: 'inherit', env: { ...process.env, ...envVars } });
 }
 
-async function buildAndPublish(platform, imageName, imageTag, dockerFile, buildArg, label, load, context, target) {
-    const dockerUser = getInput('dockerUser', { required: true });
-    const dockerPassword = getInput('dockerPassword', { required: true });
-    const dockerServer = getInput('dockerServer', { required: false });
-
-    info('Running login')
-    await executeShellScript('docker_login', {
-        INPUT_DOCKER_USER: dockerUser,
-        INPUT_DOCKER_PASSWORD: dockerPassword,
-        INPUT_DOCKER_SERVER: dockerServer,
-    });
-    await buildOnly(platform, imageName, imageTag, dockerFile, buildArg, label, load, context, target);
+function extractInputs() {
+    return {
+        imageName: getInput('imageName', { required: true }),
+        imageTag: getInput('tag'),
+        dockerFile: getInput('dockerFile'),
+        publish: getInput('publish').toLowerCase() === 'true',
+        load: getInput('load').toLowerCase() === 'true',
+        platform: getInput('platform'),
+        buildArg: getInput('buildArg'),
+        target: getInput('target'),
+        label: getInput('label'),
+        context: getInput('context'),
+    }
 }
 
-async function buildOnly(platform, imageName, imageTag, dockerFile, buildArg, label, load, context, target) {
+async function build() {
+    const { platform, imageName, imageTag, dockerFile, publish, buildArg, label, load, context, target } = extractInputs();
+
+    if (publish) {
+        const dockerUser = getInput('dockerUser', { required: true });
+        const dockerPassword = getInput('dockerPassword', { required: true });
+        const dockerServer = getInput('dockerServer', { required: false });
+
+        info('Running login')
+        await executeShellScript('docker_login', {
+            INPUT_DOCKER_USER: dockerUser,
+            INPUT_DOCKER_PASSWORD: dockerPassword,
+            INPUT_DOCKER_SERVER: dockerServer,
+        });
+    }
+
     info('Running build')
     await executeShellScript('docker_build', {
         INPUT_PLATFORM: platform,
         INPUT_IMAGE_NAME: imageName,
         INPUT_TAG: imageTag,
         INPUT_DOCKERFILE: dockerFile,
-        INPUT_PUSH: 'false',
+        INPUT_PUSH: String(publish),
         INPUT_BUILD_ARG: buildArg,
         INPUT_LABEL: label,
         INPUT_LOAD: String(load),
